42e6b7003c163b8482d35c91fd04fd8501110e9f
Miloslav Trmač <mitr@redhat.com>
2013-03-25 23:49:50 +0100

    Drop all admin identity code from pkla-check-authorization

--- src/polkitbackend/pkla-check-authorization.c
+++ src/polkitbackend/pkla-check-authorization.c
@@ -33,7 +33,6 @@
 #define _POLKIT_COMPILATION
 #define _POLKIT_BACKEND_COMPILATION
 #include <polkit/polkit.h>
-#include "polkitbackendconfigsource.h"
 #include "polkitbackendlocalauthority.h"
 #include "polkitbackendlocalauthorizationstore.h"
 
@@ -53,21 +52,12 @@
 
 /* ---------------------------------------------------------------------------------------------------- */
 
-static GList *get_users_in_group (PolkitIdentity              *group,
-                                  gboolean                     include_root);
-
-static GList *get_users_in_net_group (PolkitIdentity          *group,
-                                      gboolean                 include_root);
-
 static GList *get_groups_for_user (PolkitIdentity              *user);
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 typedef struct
 {
-  gchar *config_path;
-  PolkitBackendConfigSource *config_source;
-
   gchar **authorization_store_paths;
   GList *authorization_stores;
   GList *authorization_store_monitors;
@@ -81,20 +71,10 @@ enum
   PROP_0,
 
   // Path overrides used for unit testing
-  PROP_CONFIG_PATH,
   PROP_AUTH_STORE_PATHS,
 };
 
 /* ---------------------------------------------------------------------------------------------------- */
-
-static GList *polkit_backend_local_authority_get_admin_auth_identities (PolkitBackendInteractiveAuthority *authority,
-                                                                        PolkitSubject                     *caller,
-                                                                        PolkitSubject                     *subject,
-                                                                        PolkitIdentity                    *user_for_subject,
-									gboolean                           subject_is_local,
-									gboolean                           subject_is_active,
-                                                                        const gchar                       *action_id,
-                                                                        PolkitDetails                     *details);
 
 static PolkitImplicitAuthorization polkit_backend_local_authority_check_authorization_sync (
                                                           PolkitBackendInteractiveAuthority *authority,
@@ -292,7 +272,6 @@ polkit_backend_local_authority_init (PolkitBackendLocalAuthority *authority)
 
   priv = POLKIT_BACKEND_LOCAL_AUTHORITY_GET_PRIVATE (authority);
 
-  priv->config_path = NULL;
   priv->authorization_store_paths = NULL;
 }
 
@@ -301,16 +280,10 @@ polkit_backend_local_authority_constructed (GObject *object)
 {
   PolkitBackendLocalAuthority *authority;
   PolkitBackendLocalAuthorityPrivate *priv;
-  GFile *config_directory;
   guint n;
 
   authority = POLKIT_BACKEND_LOCAL_AUTHORITY (object);
   priv = POLKIT_BACKEND_LOCAL_AUTHORITY_GET_PRIVATE (authority);
-
-  g_debug ("Using config directory `%s'", priv->config_path);
-  config_directory = g_file_new_for_path (priv->config_path);
-  priv->config_source = polkit_backend_config_source_new (config_directory);
-  g_object_unref (config_directory);
 
   add_all_authorization_stores (authority);
 
@@ -367,10 +340,6 @@ polkit_backend_local_authority_finalize (GObject *object)
 
   g_list_free_full (priv->authorization_store_monitors, g_object_unref);
 
-  if (priv->config_source != NULL)
-    g_object_unref (priv->config_source);
-
-  g_free (priv->config_path);
   g_strfreev (priv->authorization_store_paths);
 
   G_OBJECT_CLASS (polkit_backend_local_authority_parent_class)->finalize (object);
@@ -405,10 +374,6 @@ polkit_backend_local_authority_set_property (GObject *object, guint property_id,
 
   switch (property_id)
     {
-      case PROP_CONFIG_PATH:
-        g_free (priv->config_path);
-        priv->config_path = g_value_dup_string (value);
-        break;
       case PROP_AUTH_STORE_PATHS:
         g_strfreev (priv->authorization_store_paths);
         priv->authorization_store_paths = g_strsplit (g_value_get_string (value), ";", 0);
@@ -437,15 +402,7 @@ polkit_backend_local_authority_class_init (PolkitBackendLocalAuthorityClass *kla
   authority_class->get_name                             = polkit_backend_local_authority_get_name;
   authority_class->get_version                          = polkit_backend_local_authority_get_version;
   authority_class->get_features                         = polkit_backend_local_authority_get_features;
-  interactive_authority_class->get_admin_identities     = polkit_backend_local_authority_get_admin_auth_identities;
   interactive_authority_class->check_authorization_sync = polkit_backend_local_authority_check_authorization_sync;
-
-  pspec = g_param_spec_string ("config-path",
-                               "Local Authority Configuration Path",
-                               "Path to directory of LocalAuthority config files.",
-                               PACKAGE_SYSCONF_DIR "/polkit-1/localauthority.conf.d",
-                               G_PARAM_CONSTRUCT_ONLY | G_PARAM_WRITABLE);
-  g_object_class_install_property (gobject_class, PROP_CONFIG_PATH, pspec);
 
   pspec = g_param_spec_string ("auth-store-paths",
                                "Local Authorization Store Paths",
@@ -456,82 +413,6 @@ polkit_backend_local_authority_class_init (PolkitBackendLocalAuthorityClass *kla
   g_object_class_install_property (gobject_class, PROP_AUTH_STORE_PATHS, pspec);
 
   g_type_class_add_private (klass, sizeof (PolkitBackendLocalAuthorityPrivate));
-}
-
-static GList *
-polkit_backend_local_authority_get_admin_auth_identities (PolkitBackendInteractiveAuthority *authority,
-                                                          PolkitSubject                     *caller,
-                                                          PolkitSubject                     *subject,
-                                                          PolkitIdentity                    *user_for_subject,
-							  gboolean                           subject_is_local,
-							  gboolean                           subject_is_active,
-                                                          const gchar                       *action_id,
-                                                          PolkitDetails                     *details)
-{
-  PolkitBackendLocalAuthority *local_authority;
-  PolkitBackendLocalAuthorityPrivate *priv;
-  GList *ret;
-  guint n;
-  gchar **admin_identities;
-  GError *error;
-
-  local_authority = POLKIT_BACKEND_LOCAL_AUTHORITY (authority);
-  priv = POLKIT_BACKEND_LOCAL_AUTHORITY_GET_PRIVATE (local_authority);
-
-  ret = NULL;
-
-  error = NULL;
-  admin_identities = polkit_backend_config_source_get_string_list (priv->config_source,
-                                                                   "Configuration",
-                                                                   "AdminIdentities",
-                                                                   &error);
-  if (admin_identities == NULL)
-    {
-      g_warning ("Error getting admin_identities configuration item: %s", error->message);
-      g_error_free (error);
-      goto out;
-    }
-
-  for (n = 0; admin_identities[n] != NULL; n++)
-    {
-      PolkitIdentity *identity;
-
-      error = NULL;
-      identity = polkit_identity_from_string (admin_identities[n], &error);
-      if (identity == NULL)
-        {
-          g_warning ("Error parsing identity %s: %s", admin_identities[n], error->message);
-          g_error_free (error);
-          continue;
-        }
-
-      if (POLKIT_IS_UNIX_USER (identity))
-        {
-          ret = g_list_append (ret, identity);
-        }
-      else if (POLKIT_IS_UNIX_GROUP (identity))
-        {
-          ret = g_list_concat (ret, get_users_in_group (identity, FALSE));
-        }
-      else if (POLKIT_IS_UNIX_NETGROUP (identity))
-        {
-          ret =  g_list_concat (ret, get_users_in_net_group (identity, FALSE));
-        }
-      else
-        {
-          g_warning ("Unsupported identity %s", admin_identities[n]);
-        }
-    }
-
-  g_strfreev (admin_identities);
-
- out:
-
-  /* default to uid 0 if no admin identities has been found */
-  if (ret == NULL)
-    ret = g_list_prepend (ret, polkit_unix_user_new (0));
-
-  return ret;
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
@@ -642,105 +523,6 @@ polkit_backend_local_authority_check_authorization_sync (PolkitBackendInteractiv
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
-
-static GList *
-get_users_in_group (PolkitIdentity                    *group,
-                    gboolean                           include_root)
-{
-  gid_t gid;
-  struct group *grp;
-  GList *ret;
-  guint n;
-
-  ret = NULL;
-
-  gid = polkit_unix_group_get_gid (POLKIT_UNIX_GROUP (group));
-  grp = getgrgid (gid);
-  if (grp == NULL)
-    {
-      g_warning ("Error looking up group with gid %d: %s", gid, g_strerror (errno));
-      goto out;
-    }
-
-  for (n = 0; grp->gr_mem != NULL && grp->gr_mem[n] != NULL; n++)
-    {
-      PolkitIdentity *user;
-      GError *error;
-
-      if (!include_root && g_strcmp0 (grp->gr_mem[n], "root") == 0)
-        continue;
-
-      error = NULL;
-      user = polkit_unix_user_new_for_name (grp->gr_mem[n], &error);
-      if (user == NULL)
-        {
-          g_warning ("Unknown username '%s' in group: %s", grp->gr_mem[n], error->message);
-          g_error_free (error);
-        }
-      else
-        {
-          ret = g_list_prepend (ret, user);
-        }
-    }
-
-  ret = g_list_reverse (ret);
-
- out:
-  return ret;
-}
-
-static GList *
-get_users_in_net_group (PolkitIdentity                    *group,
-                        gboolean                           include_root)
-{
-  const gchar *name;
-  GList *ret;
-
-  ret = NULL;
-  name = polkit_unix_netgroup_get_name (POLKIT_UNIX_NETGROUP (group));
-
-  if (setnetgrent (name) == 0)
-    {
-      g_warning ("Error looking up net group with name %s: %s", name, g_strerror (errno));
-      goto out;
-    }
-
-  for (;;)
-    {
-      char *hostname, *username, *domainname;
-      PolkitIdentity *user;
-      GError *error = NULL;
-
-      if (getnetgrent (&hostname, &username, &domainname) == 0)
-        break;
-
-      /* Skip NULL entries since we never want to make everyone an admin
-       * Skip "-" entries which mean "no match ever" in netgroup land */
-      if (username == NULL || g_strcmp0 (username, "-") == 0)
-        continue;
-
-      /* TODO: Should we match on hostname? Maybe only allow "-" as a hostname
-       * for safety. */
-
-      user = polkit_unix_user_new_for_name (username, &error);
-      if (user == NULL)
-        {
-          g_warning ("Unknown username '%s' in unix-netgroup: %s", username, error->message);
-          g_error_free (error);
-        }
-      else
-        {
-          ret = g_list_prepend (ret, user);
-        }
-    }
-
-  ret = g_list_reverse (ret);
-
- out:
-  endnetgrent ();
-  return ret;
-}
-
 
 static GList *
 get_groups_for_user (PolkitIdentity *user)
